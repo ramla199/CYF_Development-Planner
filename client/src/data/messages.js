09b38aff34beec72214996f01e57edc4373aa28c
hachi-ops <g.helen.rog@gmail.com>
2023-01-02 12:15:17 +0000

    again dummy file

---
+++ client/src/data/messages.js
@@ -0,0 +1,22 @@
+export const messages = [
+  {
+    id: 1,
+    sender: "ali",
+    message: "message 1",
+  },
+  {
+    id: 2,
+    sender: "delroy",
+    message: "message 2",
+  },
+  {
+    id: 3,
+    sender: "lorenzo",
+    message: "message 3",
+  },
+  {
+    id: 4,
+    sender: "barath",
+    message: "message 4",
+  },
+];
