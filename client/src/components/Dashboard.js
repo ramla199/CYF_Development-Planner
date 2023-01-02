8eaa0e3dcaba8bf7e791a542707ac6a6e4cb4cd6
hachi-ops <g.helen.rog@gmail.com>
2023-01-02 12:08:26 +0000

    split into two categories and routing them

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import messagesIcon72 from "../images/messages-icon-72.png";
+import filesIcon72 from "../images/files-icon-72.png";
+import { Link } from "react-router-dom";
 
 function Dashboard({ setAuth }) {
   const [name, setName] = useState("");
@@ -28,10 +31,26 @@ function Dashboard({ setAuth }) {
   useEffect(() => {
     getName();
   }, []);
+
   return (
     <>
-      <h1>Dashboard {name}</h1>
-      <button onClick={(e) => logout(e)}>Logout</button>
+      <header className="dash-header">
+        {" "}
+        <h1>Dashboard {name}</h1>
+        <button onClick={(e) => logout(e)}>Logout</button>
+      </header>
+      <nav className="inbox-and-files">
+        <Link to="/inbox">
+          {" "}
+          <h2>Inbox</h2>
+          <img src={messagesIcon72} alt="messages icon" />
+        </Link>
+        <Link to="/files">
+          {" "}
+          <h2>Files</h2>
+          <img src={filesIcon72} alt="files icon" />
+        </Link>
+      </nav>
     </>
   );
 }
