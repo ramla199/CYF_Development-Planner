f56d67145fc0517927c58a1b44fa767b792d62e0
hachi-ops <g.helen.rog@gmail.com>
2023-01-11 22:11:57 +0000

    revert

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get("/", (req, res) => {
 // });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build"));
 });
 
 app.listen(PORT, () => {
