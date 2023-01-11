1434ab0bbf54d1a317ea855ccf2ec11beda7f6ea
hachi-ops <g.helen.rog@gmail.com>
2023-01-11 20:57:14 +0000

    solve merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 // import FeedbacksForm from "./components/dashboard/FeedbacksForm";
 //components
 import Plans from "./components/dashboard/Plans";
 import PlanEditor from "./components/dashboard/PlanEditor";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 
 function App() {
-  const [username, setUsername] = useState(null);
+  // const [username, setUsername] = useState(null);
 
   // let location = useLocation();
   // console.log(location);
@@ -18,33 +18,6 @@ function App() {
       {/* {location.pathname !== "/plans" && <Navbar />} */}
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="register"
-          //        element={ <Register setAuth={setAuth} /> } />
-          element={<Register setUsername={setUsername} />}
-        />
-        <Route
-          exact
-          path="login"
-          //        element={ <Login setAuth={setAuth} /> } />
-          element={<Login setUsername={setUsername} />}
-        />
-        <Route
-          exact
-          path="dashboard"
-          //        element={ <Dashboard setAuth={setAuth} /> } />
-          element={<Dashboard setUsername={setUsername} />}
-        />
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NoMatch />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="inbox" element={<Inbox />} />
-        <Route path="messages" element={<Messages />} />
-        <Route path="messages/:messageid" element={<Message />} />
-        <Route path="files" element={<Files />} />
-        <Route path="new-feedback" element={<NewFeedback />} />
-        <Route path="new-message" element={<NewMessage />} />
         <Route path="plans" element={<Plans />} />
         <Route path="plan-editor" element={<PlanEditor />} />
       </Routes>
