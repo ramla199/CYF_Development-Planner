4431478d60eaf694643eb2a5b11d8d97d10d6632
hachi-ops <g.helen.rog@gmail.com>
2023-01-13 14:47:13 +0000

    app

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,23 @@ import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-//components
-import Home from "./components/Home";
+// COMPONENTS
+
+// navbar (rendered on every page)
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
 import Plans from "./components/dashboard/Plans";
 import PlanEditor from "./components/dashboard/PlanEditor";
-import Navbar from "./components/Navbar";
-import Dashboard from "./components/Dashboard";
-import { AuthProvider } from "./components/auth";
-import Login from "./components/Login";
-import RequireAuth from "./components/RequireAuth";
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="plans" element={<Plans />} />
-          <Route path="plan-editor" element={<PlanEditor />} />
-          <Route
-            path="dashboard"
-            element={
-              <RequireAuth>
-                <Dashboard />
-              </RequireAuth>
-            }
-          />
-          <Route path="login" element={<Login />} />
-        </Routes>
-      </AuthProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="plans" element={<Plans />} />
+        <Route path="plan-editor" element={<PlanEditor />} />
+      </Routes>
     </>
   );
 }
