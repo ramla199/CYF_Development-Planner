fb73632a8118388264cce86345b77ecee70d91c3
hachi-ops <g.helen.rog@gmail.com>
2023-01-15 00:32:59 +0000

    dashboard component

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+
+function Dashboard({ setAuth }) {
+  const [name, setName] = useState("");
+
+  const getName = async () => {
+    try {
+      const res = await fetch("/dashboard/", {
+        method: "GET",
+        headers: { jwt_token: localStorage.token },
+      });
+
+      const parseRes = await res.json();
+
+      console.log(parseRes);
+      setName(parseRes.username);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const logout = async (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setAuth(false);
+  };
+
+  useEffect(() => {
+    getName();
+  }, []);
+  return (
+    <>
+      <h1>Dashboard {name}</h1>
+      <button onClick={(e) => logout(e)}>Logout</button>
+    </>
+  );
+}
+
+export default Dashboard;
