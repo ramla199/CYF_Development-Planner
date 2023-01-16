0f98055ce6b384007d67450afe68c2fdbf0e09ed
hachi-ops <g.helen.rog@gmail.com>
2023-01-16 19:16:22 +0000

    another try

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ app.use(express.json());
 
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 }
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //   res.json({ message: "Hello World!" });
