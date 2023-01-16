f683293910ec5daaa700f445ec7187dea7cf055e
hachi-ops <g.helen.rog@gmail.com>
2023-01-16 03:55:55 +0000

    render link and icon

--- client/src/components/navbar/NavbarLink.js
+++ client/src/components/navbar/NavbarLink.js
@@ -1,13 +1,18 @@
 import React from "react";
 import homeIcon from "../../images/icons8-home-page-24.png";
 import { NavLink } from "react-router-dom";
-
+import dashIcon from "../../images/icons8-dashboard-48.png";
 function NavbarLink() {
   return (
     <>
       <NavLink to="/" className="nav-link">
         <img alt="home nav link icon" src={homeIcon} />
         <p className="icon-p">Home</p>
+      </NavLink>
+
+      <NavLink to="dashboard" className="nav-link">
+        <img alt="dashboard nav link icon" src={dashIcon} />
+        <p className="icon-p">Dashboard</p>
       </NavLink>
     </>
   );
