287809071392ac337c5dad1e57cb3a330b56f6ad
hachi-ops <g.helen.rog@gmail.com>
2023-01-16 19:01:32 +0000

    deployment

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ if (process.env.NODE_ENV === "production") {
 //   res.json({ message: "Hello World!" });
 // });
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build"));
 });
