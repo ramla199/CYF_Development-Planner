2a85f4617d44b89c3099810bfe34fed6f5e8c818
hachi-ops <g.helen.rog@gmail.com>
2023-01-17 16:04:51 +0000

    remove comments

--- index.js
+++ index.js
@@ -7,26 +7,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, "client/build")));
-
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
 }
 
-// app.use(express.static(path.join(__dirname, 'build')));
-
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "Hello World!" });
-// });
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./client/build"));
-// });
 
 //routes
 
