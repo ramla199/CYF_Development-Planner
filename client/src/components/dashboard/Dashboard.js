7a61da0990ca233d15b1048a256835736145237c
hachi-ops <g.helen.rog@gmail.com>
2023-02-03 06:02:11 +0000

    new components/deletions

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,61 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import Logout from "./Logout";
 
 // components
 
 import Files from "./mentor/Files";
 import Inbox from "./mentor/Inbox";
-
-import NewMessage from "./mentor/NewMessage";
+import Name from "./Name";
 
 function Dashboard({ setAuth }) {
-  const [name, setName] = useState("");
-
-  const navigate = useNavigate();
-
-  const getName = async () => {
-    try {
-      const res = await fetch("/dashboard/", {
-        method: "GET",
-        headers: { jwt_token: localStorage.token },
-      });
-
-      const parseRes = await res.json();
-
-      console.log(parseRes);
-
-      // setName(parseRes.username);
-
-      setName(parseRes.username);
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-
-  useEffect(() => {
-    getName();
-  }, []);
-
-  const logout = async (e) => {
-    e.preventDefault();
-    localStorage.removeItem("token");
-    localStorage.removeItem("port");
-    localStorage.removeItem("username");
-    localStorage.removeItem("role");
-    setAuth(false);
-  };
-
-  useEffect(() => {
-    getName();
-  }, []);
-
   return (
     <>
       <section>
-        <h1 className="heading db-heading">{name}</h1>
-        <div className="login-signin-buttons">
-          <button onClick={(e) => logout(e)}>Logout</button>
-        </div>
+        <Name />
+        <Logout setAuth={setAuth} />
         <Files />
         <Inbox />
       </section>
