ed6281e7bd44f895db52287ae1819d968028b8e9
hachi-ops <g.helen.rog@gmail.com>
2023-02-03 06:07:35 +0000

    minor

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 }
 
 // Routes
