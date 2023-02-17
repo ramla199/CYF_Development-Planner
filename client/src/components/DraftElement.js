033ce849f0731038662772b7456bc12152196503
hachi-ops <g.helen.rog@gmail.com>
2023-02-17 04:15:20 +0000

    add component

--- client/src/components/DraftElement.js
+++ client/src/components/DraftElement.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-function DraftElement({ draft }) {
+function DraftElement({ draft, deleteDraft }) {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+
+  const handleSend = () => {
+    console.log("this draft will be sent");
+  };
+
   return (
     <>
       <div key={draft.draft_id}>
         <div>{draft.draft_title}</div>
-        <div>{draft.draft_text}</div>
+        <button onClick={handleClick}>open</button>
+        {toggle ? (
+          <div>
+            {draft.draft_text}
+            <button onClick={() => deleteDraft(draft.draft_id)}>Delete</button>
+            <button onClick={handleSend}>send</button>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
