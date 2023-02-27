1121a4f0909b03cd50f1d837a549937dcb96f817
hachi-ops <g.helen.rog@gmail.com>
2023-02-27 21:54:46 +0000

    restore mentors dropdown component (previously added)

--- client/src/components/dashboard/Element.js
+++ client/src/components/dashboard/Element.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import EditDraft from "./EditDraft";
+import MentorsDropdown from "./MentorsDropdown";
 
 function Element({ draft, deleteDraft, setDraftsChange }) {
   const [toggle, setToggle] = useState(false);
@@ -13,14 +14,44 @@ function Element({ draft, deleteDraft, setDraftsChange }) {
     setShowMentors(!showMentors);
   }
 
+  const [mentorName, setMentorName] = useState("");
+  const [allMentors, setAllMentors] = useState([]);
+  const getMentorName = async () => {
+    try {
+      const res = await fetch("/dashboard/mentors", {
+        method: "GET",
+        headers: { jwt_token: localStorage.token },
+      });
+
+      const parseRes = await res.json();
+
+      console.log(parseRes);
+      setMentorName(parseRes.username);
+      setAllMentors(allMentors);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getMentorName();
+  }, []);
   return (
     <>
       <section>
         <div className="flex" key={`elem-${draft.draft_id}`}>
           <button onClick={handleEdit}>edit</button>
           <button onClick={() => deleteDraft(draft.draft_id)}>Delete</button>
-          <button onClick={handleSend}>send</button>
-        </div>{" "}
+        </div>
+        <section>
+          <section>
+            {" "}
+            <button onClick={handleSend}>send</button>{" "}
+            {showMentors ? <MentorsDropdown /> : <></>}
+          </section>
+
+          {draft.draft_text}
+        </section>
       </section>
 
       {toggle ? (
